bf55c8a58ca87ab931815719200e23dfbb903c5d
Daniel Weinman <namniewd@gmail.com>
2019-03-09 09:39:40 -0500

    basic server setup, begun work on auth routes

---
+++ api/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const authRouter = require('../auth/authRouter.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.use('/api/auth', authRouter);
+
+
+server.get('/', (req, res) => {
+    res.send('It\'s Working!');
+});
+
+
+
+
+
+module.exports = server;
---
+++ auth/authRouter.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+
+
+
+module.exports = router;
