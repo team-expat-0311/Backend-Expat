64d2ce07902caeb7f14d4700c4786c65eb9f75c4
Daniel Weinman <namniewd@gmail.com>
2019-03-11 13:07:07 -0400

    updated add() user function for knex/pg

--- users/usersModel.js
+++ users/usersModel.js
@@ -7,7 +7,7 @@ module.exports = {
 }
 
 async function add(user) {
-    const [id] = await db('users').insert(user);
+    const [id] = await db('users').insert(user, 'id');
 
     return findById(id);
 }
